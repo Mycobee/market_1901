ff6527f1887137c9752d167fa501d16b1f3c08e7
Rob Stringer <stringer525@gmail.com>
2019-02-20 10:49:44 -0700

    Finished iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,51 @@
 class Market
+  attr_reader :name,
+              :vendors
+
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map {|vendor| vendor.name}
+  end
+
+  def vendors_that_sell(item)
+    vendor_sellers = @vendors.select {|vendor| vendor.inventory.keys.include?(item)}
+
+  end
+
+  def sorted_item_list
+    inventories = vendors.map {|vendor| vendor.inventory}
+    inventory_names = inventories.map {|item_hash| item_hash.keys}.flatten
+    sorted_item_list = inventory_names.uniq.sort
+  end
+
+  def total_inventory
+    total_inventory = {}
+    @vendors.each do |vendor|
+      vendor.inventory.keys.each do |key|
+        if !total_inventory.keys.include?(key)
+          total_inventory[key] = 0
+        end
+      end
+      total_inventory
+    end
+
+    @vendors.each do |vendor|
+      vendor.inventory.each do |inventory_key, inventory_value|
+        total_inventory[inventory_key] += inventory_value
+      end
+      total_inventory
+    end
+
+    total_inventory
+  end
+
 
 end
--- test/market_test.rb
+++ test/market_test.rb
@@ -2,7 +2,80 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/market'
 require './lib/vendor'
+require 'pry'
 
 class MarketTest < Minitest::Test
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+    @vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor_1.stock("Peaches", 35)
+    @vendor_1.stock("Tomatoes", 7)
+    @vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor_2.stock("Banana Nice Cream", 50)
+    @vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    @vendor_3 = Vendor.new("Palisade Peach Shack")
+    @vendor_3.stock("Peaches", 65)
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+
+  def test_it_has_a_name
+    assert_equal "South Pearl Street Farmers Market", @market.name
+  end
+
+  def test_it_start_with_an_empty_array_of_vendors
+    assert_equal [], @market.vendors
+  end
+
+  def test_add_vendor_method
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    expected = [@vendor_1, @vendor_2, @vendor_3]
+    actual = @market.vendors
+  end
+
+  def test_vendor_names
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+
+    expected = ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"]
+    actual = @market.vendor_names
+  end
+
+  def test_vendors_that_sell_method
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    expected_1 = [@vendor_1, @vendor_3]
+    actual_1 = @market.vendors_that_sell("Peaches")
+    assert_equal expected_1, actual_1
+
+    expected_2 = [@vendor_2]
+    actual_2 = @market.vendors_that_sell("Banana Nice Cream")
+    assert_equal expected_2, actual_2
+  end
+
+  def test_sorted_item_list
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    expected = ["Banana Nice Cream", "Peach-Raspberry Nice Cream", "Peaches", "Tomatoes"]
+    actual = @market.sorted_item_list
+    assert_equal expected, actual
+  end
+
+  def test_total_inventory
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    expected = {"Peaches"=>100, "Tomatoes"=>7, "Banana Nice Cream"=>50, "Peach-Raspberry Nice Cream"=>25}
+    actual = @market.total_inventory
+    assert_equal expected, actual
+  end
+
 
 end
