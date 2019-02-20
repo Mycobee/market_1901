6dc06b6ce7afa861fce3b09fb47274fe6f364249
Rob Stringer <stringer525@gmail.com>
2019-02-20 09:51:56 -0700

    Finished iteration 1 and cooresponding tests

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -2,9 +2,20 @@ class Vendor
   attr_reader :name,
               :inventory
 
+
   def initialize(name)
     @name = name
-    @inventory = # you decide what goes here...
+    @inventory = {}
   end
 
+  def check_stock(item)
+      if @inventory[item] == nil
+        @inventory[item] = 0
+      end
+      @inventory[item]
+  end
+
+  def stock(name, count)
+     @inventory[name] = count + @inventory[name].to_i
+  end
 end
--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/vendor'
+require 'pry'
 
 class VendorTest < Minitest::Test
   def test_it_exists
@@ -16,6 +17,32 @@ class VendorTest < Minitest::Test
   def test_inventory_starts_as_empty_hash
     vendor = Vendor.new("Rocky Mountain Fresh")
     assert_equal ({}), vendor.inventory
+  end
+
+  def test_check_stock
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    expected = 0
+    actual = vendor.check_stock("Peaches")
+    assert_equal expected, actual
+  end
+
+  def test_stock_method_corresponds_with_check_stock_and_inventory
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    actual_1 = vendor.stock("Peaches", 30)
+    expected_1 = 30
+    vendor.check_stock("Peaches")
+
+    assert_equal expected_1, actual_1
+     # binding.pry
+    vendor.stock("Peaches", 25)
+    expected_2 = 55
+    actual_2 = vendor.check_stock("Peaches")
+    assert_equal expected_2, actual_2
+
+    vendor.stock("Tomatoes", 12)
+    expected_3 = {"Peaches"=>55, "Tomatoes"=>12}
+    actual_3 = vendor.inventory
+    assert_equal expected_3, actual_3
   end
 
 end
